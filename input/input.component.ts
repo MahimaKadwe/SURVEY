41ea30224bce6b4655c3accb361d8bfeb132dc88
Mahima Kadwe <mahimakadwe@gmail.com>
2023-02-13 13:49:43 +0530

    first commit

---
+++ input/input.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+// import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+// import {ErrorStateMatcher} from '@angular/material/core';
+
+// export class MyErrorStateMatcher implements ErrorStateMatcher {
+//   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+//     const isSubmitted = form && form.submitted;
+//     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+//   }
+// }
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent {
+  value = 'Clear me';
+  constructor() { }
+  text :any;
+  ngOnInit(): void {
+
+  }
+
+  }
+
+
